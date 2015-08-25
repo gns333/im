49943bf861e0e243c6e38eb7b735d5cb6234f533
gns333 <gns3@foxmail.com>
2015-08-25 20:02:54 +0800

    add mgo

--- src/dbexecutor/mangodb.go
+++ src/dbexecutor/mangodb.go
@@ -5,22 +5,77 @@ import (
 	_ "labix.org/v2/mgo/bson"
 )
 
-func Init() {
+var mgoexec *mgoSessions
 
+const (
+	DBUrl     = ""
+	DBConnNum = 10
+)
+
+func Init() {
+	mgoSessions.open(DBUrl, DBConnNum)
 }
 
 func FindOne(collName string, query interface{}, result interface{}) error {
+	ses := mgoSessions.fetch()
+	defer mgoSessions.push(ses)
 
+	return ses.C(collName).Find(query).One(result)
 }
 
 func FindAll(collName string, query interface{}, result interface{}) error {
+	ses := mgoSessions.fetch()
+	defer mgoSessions.push(ses)
 
+	return ses.C(collName).Find(query).All(result)
 }
 
 func Insert(collName string, document interface{}) error {
+	ses := mgoSessions.fetch()
+	defer mgoSessions.push(ses)
 
+	return ses.C(collName).Insert(document)
 }
 
-func Update(collName string, selector interface{}, update interface{}) {
+func Update(collName string, selector interface{}, update interface{}) error {
+	ses := mgoSessions.fetch()
+	defer mgoSessions.push(ses)
 
+	return ses.C(collName).Update(selector, update)
+}
+
+type mgoSessions struct {
+	isInit      bool
+	sessionChan chan *mgo.Session
+}
+
+func (self *mgoSessions) open(connectUrl string, sessionCount int) error {
+
+	self.isInit = true
+	self.sessionChan = make(chan *mgo.Session, sessionCount)
+
+	for i := 0; i < sessionCount; i++ {
+		session, err := mgo.Dial(connectUrl)
+		if err != nil {
+			return err
+		}
+		session.SetMode(mgo.Monotonic, true)
+		self.sessionChan <- &session
+	}
+}
+
+func (self *mgoSessions) fetch() *mgo.Session {
+	return <-self.sessionChan
+}
+
+func (self *mgoSessions) push(ses *mgo.Session) {
+	self.sessionChan <- ses
+}
+
+func (self *mgoSessions) close() {
+	for i := 0; i < cap(self.sessionChan); i++ {
+		ses <- self.sessionChan
+		ses.Close()
+	}
+	self.isInit = false
 }
